8771f1a17c63051995ac8b5997ff8f84934db06f
Gautam Malu <gautam.maloo@gmail.com>
2017-08-16 14:30:40 +0530

    Updated header files for OpenCV3 compatibility

--- include/caffe/ACTION_REC/video_data_layer.hpp
+++ include/caffe/ACTION_REC/video_data_layer.hpp
@@ -14,11 +14,20 @@
 #include "caffe/util/blocking_queue.hpp"   
 #include "caffe/layers/base_data_layer.hpp"
 #include "caffe/proto/caffe.pb.h"
+#include "opencv2/core/version.hpp"
 
+#if CV_MAJOR_VERSION == 2
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/contrib/contrib.hpp>
+
+#elif CV_MAJOR_VERSION == 3
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#endif
 
 namespace caffe {
 
--- src/caffe/FRCNN/frcnn_roi_data_layer.cpp
+++ src/caffe/FRCNN/frcnn_roi_data_layer.cpp
@@ -7,9 +7,19 @@
 #include <utility>
 #include <vector>
 
+#include "opencv2/core/version.hpp"
+#if CV_MAJOR_VERSION == 2
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+
+#elif CV_MAJOR_VERSION == 3
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+#endif
+
 
 #include "caffe/data_transformer.hpp"
 #include "caffe/internal_thread.hpp"
